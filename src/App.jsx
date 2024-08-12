3895a7cefab0254d96059f10d4dd8876339dde89
Mary Rose Antonio <146495872+maryrosegithub21@users.noreply.github.com>
2024-08-12 13:16:10 +1200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,8 @@ function App() {
     <>
      <Routes>
      <Route path='/' element={<Home />} />
+    <Route path='/mcgi-feast-portal' element={<Home />} />
+     
      <Route path='/mcgi-feast-portal-log-in' element={<Login />} />
      <Route path='/mcgi-feast-portal-sign-up' element={<SignUp />} />
      {/* <Route path='/MCGI-BRETHREN-DAY-NZ' element={<HeroBanner />} />
